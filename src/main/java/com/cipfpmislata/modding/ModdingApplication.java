54b5d6542684b0ad40c5dc44ad9288165cdc3f8a
XaviRibera <xaviribera2004@gmail.com>
2024-01-12 12:44:38 +0100

    Arquitectura creada

---
+++ src/main/java/com/cipfpmislata/modding/ModdingApplication.java
@@ -0,0 +1,13 @@
+package com.cipfpmislata.modding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModdingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModdingApplication.class, args);
+	}
+
+}
