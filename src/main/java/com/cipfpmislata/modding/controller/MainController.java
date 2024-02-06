ffa3c15d9f26955a848405b8d01a875ca55cd467
XaviRibera <xaviribera2004@gmail.com>
2024-02-06 11:58:09 +0100

    06/02/2024

--- src/main/java/com/cipfpmislata/modding/controller/MainController.java
+++ src/main/java/com/cipfpmislata/modding/controller/MainController.java
@@ -1,5 +1,17 @@
 package com.cipfpmislata.modding.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/mods")
+@RestController
 public class MainController {
-    
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("")
+    public String main(){
+        return "FUNCIONA";
+    }
 }
